512b176efddd9db0b701efdb0d9bd921f8a4baad
Danny <dq7@me.com>
2023-09-29 23:28:09 +0100

    Basic Expression Parser

---
+++ bytecode/chunk.h
@@ -0,0 +1,38 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum
+{
+	OP_ADD,
+	OP_CONSTANT,
+	OP_DIVIDE,
+	OP_FALSE,
+	OP_MULTIPLY,
+	OP_NEGATE,
+	OP_NIL,
+	OP_NOT,
+	OP_RETURN,
+	OP_SUBTRACT,
+	OP_TRUE
+} OpCode;
+
+typedef struct {
+	int count;
+	int capacity;
+	uint8_t* code;
+	int* lines;
+	ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
+int addConstant(Chunk* chunk, Value value);
+
+void freeChunk(Chunk* chunk);
+
+#endif 
