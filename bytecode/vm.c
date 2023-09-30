178d5e078e74b97d7aeb18c20cf2ffb7177c7838
Danny <dq7@me.com>
2023-09-30 16:34:38 +0100

    Equality & Comparison

--- bytecode/vm.c
+++ bytecode/vm.c
@@ -49,7 +49,7 @@ Value pop()
 }
 
 static Value peek(int distance) {
-	return vm.stack[-1 - distance];
+	return vm.stackTop[-1 - distance];
 }
 
 static bool isFalsey(Value value) {
@@ -92,6 +92,8 @@ static InterpretResult run()
 			case OP_DIVIDE:		BINARY_OP(NUMBER_VAL, /); break;
 			case OP_MULTIPLY:	BINARY_OP(NUMBER_VAL, *); break;
 			case OP_SUBTRACT:	BINARY_OP(NUMBER_VAL, -); break;
+			case OP_GREATER:	BINARY_OP(BOOL_VAL, >); break; 
+			case OP_LESS:		BINARY_OP(BOOL_VAL, <); break;
 			case OP_NIL:		push(NIL_VAL); break;
 			case OP_TRUE:		push(BOOL_VAL(true)); break;
 			case OP_FALSE:		push(BOOL_VAL(false)); break;
@@ -109,6 +111,13 @@ static InterpretResult run()
 				}
 				push(NUMBER_VAL(-AS_NUMBER(pop())));
 				break;
+			case OP_EQUAL: {
+				Value b = pop();
+				Value a = pop();
+
+				push(BOOL_VAL(valuesEqual(a, b)));
+				break;
+			}
 			case OP_RETURN:
 				printValue(pop());
 				printf("\n");
