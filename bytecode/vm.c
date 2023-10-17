d0e7ff81f85d2f2519dbfd74b0d15c44b8acec36
Danny <dq7@me.com>
2023-10-17 21:07:41 +0100

    Basic global variables.

--- bytecode/vm.c
+++ bytecode/vm.c
@@ -32,11 +32,16 @@ static void runtimeError(const char* format, ...) {
 void initVM()
 {
 	resetStack();
+	vm.objects = NULL;
+	initTable(&vm.globals);
+	initTable(&vm.strings);
 }
 
 void freeVM()
 {
-
+	freeTable(&vm.globals);
+	freeTable(&vm.strings);
+	freeObjects();
 }
 
 void push(Value value)
@@ -80,14 +85,15 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
 	do { \
 		if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
 			runtimeError("Operands must be numeric."); \
 			return INTERPRET_RUNTIME_ERROR; \
 		} \
-		double a = AS_NUMBER(pop()); \
 		double b = AS_NUMBER(pop()); \
+		double a = AS_NUMBER(pop()); \
 		push(valueType(a op b)); \
 	} while (false)
 
@@ -133,6 +139,24 @@ static InterpretResult run()
 			case OP_TRUE:		push(BOOL_VAL(true)); break;
 			case OP_FALSE:		push(BOOL_VAL(false)); break;
 			case OP_NOT:		push(BOOL_VAL(isFalsey(pop()))); break;
+			case OP_POP:		pop(); break;
+			case OP_GET_GLOBAL: {
+				ObjString* name = READ_STRING();
+				Value value; 
+				if (!tableGet(&vm.globals, name, &value)) {
+					runtimeError("Undefined variable '%s'.", name->chars);
+
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				push(value);
+				break;
+			}
+			case OP_DEFINE_GLOBAL: {
+				ObjString* name = READ_STRING(); 
+				tableSet(&vm.globals, name, peek(0));
+				pop();
+				break;
+			}
 			case OP_CONSTANT:
 			{
 				Value constant = READ_CONSTANT();
@@ -153,13 +177,17 @@ static InterpretResult run()
 				push(BOOL_VAL(valuesEqual(a, b)));
 				break;
 			}
-			case OP_RETURN:
+			case OP_PRINT: {
 				printValue(pop());
 				printf("\n");
+				break;
+			}
+			case OP_RETURN:
 				return INTERPRET_OK;
 		}
 	}
 #undef BINARY_OP
+#undef READ_STRING
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
