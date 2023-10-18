be7efc64064935d32929d82e4c3057478bfbfe06
Danny <dq7@me.com>
2023-10-18 23:08:25 +0100

    if statement

--- bytecode/compiler.c
+++ bytecode/compiler.c
@@ -137,6 +137,23 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
 	emitByte(byte2);
 }
 
+static void patchJump(int offset) {
+	int jump = currentChunk()->count - offset - 2;
+	if (jump > UINT16_MAX) {
+		error("Too much code to jump over.");
+	}
+
+	currentChunk()->code[offset] = (jump >> 8) & 0xff;
+	currentChunk()->code[offset + 1] = jump & 0xff;
+}
+
+static int emitJump(uint8_t instruction) {
+	emitByte(instruction);
+	emitByte(0xff);
+	emitByte(0xff);
+	return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
 	emitByte(OP_RETURN);
 }
@@ -329,6 +346,25 @@ static void expressionStatement() {
 	emitByte(OP_POP);
 }
 
+static void ifStatement() {
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+	int thenJump = emitJump(OP_JUMP_IF_FALSE);
+	emitByte(OP_POP);
+	statement();
+
+	int elseJump = emitJump(OP_JUMP);
+
+	patchJump(thenJump);
+	emitByte(OP_POP);
+
+	if (match(TOKEN_ELSE)) statement();
+
+	patchJump(elseJump);
+}
+
 static void printStatement() {
 	expression(); 
 
@@ -362,6 +398,9 @@ static void synchronize() {
 static void declaration() {
 	if (match(TOKEN_VAR)) {
 		varDeclaration(); 
+	}
+	else if (match(TOKEN_IF)) {
+		ifStatement();
 	}
 	else {
 		statement();
